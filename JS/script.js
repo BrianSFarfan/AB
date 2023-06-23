19f2fde9d3525f9e5ae81a80702e4e075cb91df5
BrianSFarfan <brian.sfarfan@gmail.com>
2023-06-23 01:08:57 -0300

    epa

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,13 @@
 var boton1 = document.getElementById("de1-2");
 var boton2 = document.getElementById("de2-3");
 var boton3 = document.getElementById("de3-4");
+var boton4 = document.getElementById("de4-5");
 // Obtén las vistas por sus IDs
 var vista1 = document.getElementById("step1");
 var vista2 = document.getElementById("step2");
 var vista3 = document.getElementById("step3");
 var vista4 = document.getElementById("step4");
+var vista5 = document.getElementById("step5");
 
 // Agrega el evento onclick al botón
 boton1.onclick = function() {
@@ -52,4 +54,19 @@ boton3.onclick = function() {
       vista3.style.display = "none";
       vista4.style.display = "block";
     }, 1000); // 1000 ms = 1 segundo
+  };
+
+   /*FUNCION 3*/
+
+boton4.onclick = function() {
+    // Oculta vista1 después de 1 segundo y muestra vista2
+    setTimeout(function() {
+    vista4.style.opacity = "0";
+      vista4.style.marginTop ="100px"
+      },1000)
+  
+    setTimeout(function() {
+      vista4.style.display = "none";
+      vista5.style.display = "block";
+    }, 1500); // 1000 ms = 1 segundo
   };
