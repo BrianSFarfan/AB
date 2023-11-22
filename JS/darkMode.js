94fd5f8e17733e607a2aa06aba50c8f64b3a1616
BrianSFarfan <brian.sfarfan@gmail.com>
2023-11-21 22:55:03 -0300

    a

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -1,6 +1,6 @@
 const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 const slider = document.getElementById('slider');
-
+const container = document.getElementById('container');
 
 const setTheme = (theme) => {
     document.documentElement.setAttribute('data-theme', theme);
@@ -11,6 +11,7 @@ slider.addEventListener('click', () => {
     let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
     setTheme(switchToTheme);
     console.log(switchToTheme);
+    
 });
 
 setTheme(localStorage.getItem('theme') || preferedColorScheme);
